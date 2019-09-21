baa7e650dc55b49c570ab68b775b6c69ab842519
Philipp Rudiger <prudiger@anaconda.com>
2019-09-21 12:41:24 +0200

    Add cuDFInterface to work with cuDF GPU dataframes

--- holoviews/core/data/__init__.py
+++ holoviews/core/data/__init__.py
@@ -49,6 +49,12 @@ try:
 except ImportError:
     pass
 
+try:
+    from .cudf import cuDFInterface   # noqa (Conditional API import)
+    datatypes.append('cuDF')
+except ImportError:
+    pass
+
 if 'array' not in datatypes:
     datatypes.append('array')
 if 'multitabular' not in datatypes:
